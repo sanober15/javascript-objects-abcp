d9fa039bd4eac79e6f50cf01000e248940ecad67
sanober riaz <sanober@sanober.me>
2018-01-15 00:58:05 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { artistName: "songTitle" };
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  playlist;
+}
